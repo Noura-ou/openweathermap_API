31e89ed298558eb174087885fe328fc83b10dccc
Noura Ous <noura.ousfia@gmail.com>
2023-04-03 13:39:35 +0200

    create the database sgl

---
+++ src/crud.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+import sqlite3
+import pandas as pd
+
+
+
+# Créer une connexion à la base de données
+connection = sqlite3.connect("weather_base.db")
+
+# Nom de la table dans la base de données
+sql_table = 'weather_data_SQL'
+
+# Charger les données dans un DataFrame
+df = pd.read_csv("weather_data.csv")
+
+# Écrire le DataFrame dans la table SQL
+df.to_sql(sql_table, connection, if_exists='replace')
+
+# Fermer la connexion à la base de données
+connection.close()
+
